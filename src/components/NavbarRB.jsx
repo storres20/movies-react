f12453c3755f2b1de0a73af13541698401800620
storres20 <sanchotorres777@gmail.com>
2022-02-09 22:46:25 -0500

    modificacion

--- src/components/NavbarRB.jsx
+++ src/components/NavbarRB.jsx
@@ -23,9 +23,9 @@ export default function NavbarRB() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link><NavLink to="/">Home</NavLink></Nav.Link>
-            <Nav.Link><NavLink to="/about">About</NavLink></Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown" >
+            <Nav.Link><NavLink to="/" className={styles.NavSubTitle}>Home</NavLink></Nav.Link>
+            <Nav.Link><NavLink to="/about" className={styles.NavSubTitle}>About</NavLink></Nav.Link>
+            <NavDropdown title="DropdownXXX" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1"><span>Action</span></NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2"><span>Another action</span></NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3"><span>Something</span></NavDropdown.Item>
