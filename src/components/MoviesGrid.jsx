342c8fed26b05a305153e67a2d6b18e361c15d66
storres20 <sanchotorres777@gmail.com>
2022-03-22 16:25:30 -0500

    fix search bar

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -7,18 +7,44 @@ export function MoviesGrid() {
   //let movies = [];
   const [movies, setMovies] = useState([]);
   
+  const [allData, setAllData] = useState([]);
+
   useEffect(() => {
     get("/discover/movie")
-    .then(data => {
-      setMovies(data.results);
-    });
+      .then(data => {
+        setMovies(data.results);
+        setAllData(data.results);
+      });
   }, []);
-  
+
+  const handleSearch = (event) => {
+    const keyword = event.target.value;
+
+    if (keyword !== '') {
+      const results = allData.filter((user) => {
+        //return user.title.toLowerCase().startsWith(keyword.toLowerCase());
+        return user.title.toLowerCase().includes(keyword.toLowerCase());
+        // Use the toLowerCase() method to make it case-insensitive
+      });
+
+      setMovies(results);
+
+    } else {
+      setMovies(allData);
+      // If the text field is empty, show all users
+    }
+
+  }
+
   return (
-    <ul className={styles.moviesGrid}>
-      {movies.map((movie) => {
-        return <MovieCard key={movie.id} movie={movie} />
-      })}
-    </ul>
-    );
+    <div>
+      <input className="bg-dark text-white ml-5 mr-5" type="text" placeholder='Search...' onChange={event => handleSearch(event)} />
+
+      <ul className={styles.moviesGrid}>
+        {movies.map((movie) => {
+          return <MovieCard key={movie.id} movie={movie} />
+        })}
+      </ul>
+    </div>
+  );
 }
