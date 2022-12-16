f3ced57d52185aa0971d96cfd395bae9a6d150c2
Italo Lon kan <sanchotorres777@gmail.com>
2022-12-16 18:40:28 -0500

    Fix React state issue

--- src/components/ScrollButton.jsx
+++ src/components/ScrollButton.jsx
@@ -1,36 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaArrowCircleUp } from 'react-icons/fa';
 import styles from './ScrollButton.module.css';
 
 function ScrollButton() {
-  const [visible, setVisible] = useState(false);
+  /* **** */
 
-  const toggleVisible = () => {
-    const scrolled = document.documentElement.scrollTop;
-    if (scrolled > 300) {
-      setVisible(true);
-    } else if (scrolled <= 300) {
-      setVisible(false);
+  // The back-to-top button is hidden at the beginning
+  const [showButton, setShowButton] = useState(false);
+
+  const scrollChange = () => {
+    if (window.pageYOffset > 300) {
+      setShowButton(true);
+    } else {
+      setShowButton(false);
     }
   };
 
+  useEffect(() => {
+    window.addEventListener('scroll', scrollChange);
+    return () => {
+      window.removeEventListener('scroll', scrollChange);
+    };
+  }, []);
+
+  // This function will scroll the window to the top
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
-      behavior: 'smooth',
-      /* you can also use 'auto' behaviour
-         in place of 'smooth' */
+      behavior: 'smooth', // for smoothly scrolling
     });
   };
 
-  window.addEventListener('scroll', toggleVisible);
-
   return (
     <div className={styles.button}>
-      <FaArrowCircleUp
-        onClick={scrollToTop}
-        style={{ display: visible ? 'inline' : 'none' }}
-      />
+      {showButton && (
+      <FaArrowCircleUp onClick={scrollToTop} />
+      )}
+
+      {/* &#8679; is used to create the upward arrow */}
     </div>
   );
 }
