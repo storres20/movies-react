d75b5e8bff26315fe374acf89f8cd6ef4b2d7859
Italo Lon kan <sanchotorres777@gmail.com>
2022-12-16 20:34:03 -0500

    Remove some elements

--- src/components/NavbarRB.jsx
+++ src/components/NavbarRB.jsx
@@ -2,7 +2,7 @@ import { NavLink } from 'react-router-dom';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import '../custom.scss';
 import {
-  Navbar, Container, Nav, NavDropdown,
+  Navbar, Container, Nav,
 } from 'react-bootstrap';
 import styles from './NavbarRB.module.css';
 
@@ -16,13 +16,6 @@ export default function NavbarRB() {
           <Nav className={`me-auto ${styles.NavBar}`}>
             <NavLink to="/" className={styles.NavSubTitle}>Home</NavLink>
             <NavLink to="/about" className={styles.NavSubTitle}>About</NavLink>
-            <NavDropdown title="DropdownXXX" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1"><span>Action</span></NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2"><span>Another action</span></NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3"><span>Something</span></NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4"><span>Separated link</span></NavDropdown.Item>
-            </NavDropdown>
           </Nav>
         </Navbar.Collapse>
       </Container>
