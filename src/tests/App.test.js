a0f586267148d59f91d48433af3e21ce8f59b624
Italo Lon kan <sanchotorres777@gmail.com>
2022-12-18 02:40:48 -0500

    Update test file

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+// Redux
+import { Provider } from 'react-redux';
+import store from '../redux/configureStore';
+// ****
 import App from '../pages/App';
 
 it('renders correctly', () => {
   const tree = renderer
     .create(
-      <App />,
+      <Provider store={store}>
+        <App />
+      </Provider>,
     )
     .toJSON();
   expect(tree).toMatchSnapshot();
