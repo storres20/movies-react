71bd87bdc55e3384d01ad890307575162872da34
Italo Lon kan <sanchotorres777@gmail.com>
2022-12-18 02:41:19 -0500

    Add configureStore for testing file

--- src/tests/MoviesGrid.test.js
+++ src/tests/MoviesGrid.test.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
 import MoviesGrid from '../components/MoviesGrid';
+import moviesReducer from '../redux/movies/movies';
 
-it('renders correctly', () => {
-  const tree = renderer
-    .create(
-      <MoviesGrid />,
-    )
-    .toJSON();
-  expect(tree).toMatchSnapshot();
+// configureStore
+const createTestStore = () => {
+  const store = configureStore({
+    reducer: {
+      movie: moviesReducer,
+    },
+  });
+
+  return store;
+};
+export default createTestStore;
+
+// Test
+describe('My Connected React-Redux Component', () => {
+  let store;
+  let component;
+
+  beforeEach(() => {
+    store = createTestStore();
+
+    // Create a redux store
+    component = renderer.create(
+      <Provider store={store}>
+        <MoviesGrid />
+      </Provider>,
+    ).toJSON();
+  });
+
+  test('renders correctly', () => {
+    expect(component).toMatchSnapshot();
+  });
 });
