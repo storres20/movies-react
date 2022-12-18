3757a0a39050b7fa02ea0643e02de496befab5a1
Italo Lon kan <sanchotorres777@gmail.com>
2022-12-17 21:04:06 -0500

    Add redux provider and store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+// Redux
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
+// ****
 import App from './pages/App';
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root'),
 );
