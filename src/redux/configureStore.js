de2f48e864bc8056e63c36921f65579290283484
storres20 <sanchotorres777@gmail.com>
2022-12-19 09:08:29 -0500

    Add allData reducer

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import moviesReducer from './movies/movies';
+import allDataReducer from './movies/allData';
 
 const store = configureStore({
   reducer: {
     movie: moviesReducer,
+    allData: allDataReducer,
   },
 });
 
