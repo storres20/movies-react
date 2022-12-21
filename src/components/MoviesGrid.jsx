ee9c0dec2286e53f04cabb1970c85576dd6f65bb
storres20 <sanchotorres777@gmail.com>
2022-12-21 17:38:05 -0500

    Remove redux setup

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -1,23 +1,10 @@
-import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import MovieCard from './MovieCard';
 import styles from './MoviesGrid.module.css';
 import ScrollButton from './ScrollButton';
-// Redux
-import { getMovie } from '../redux/movies/movies';
-import { getAllData } from '../redux/movies/allData';
 import SearchBar from './SearchBar';
-// ***
 
 export default function MoviesGrid() {
-  /* Setup Redux dispatch */
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getMovie());
-    dispatch(getAllData());
-  }, [dispatch]);
-
   /* Load Redux State */
   const movies = useSelector((state) => state.movie);
   const allData = useSelector((state) => state.allData);
